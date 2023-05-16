478c47364fff3540d6961b15144918ee5f4ee893
Muhammad Nizar <nizares234@gmail.com>
2023-05-16 17:40:19 +0800

    Adding with Google Cloud Storages

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import os
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, url_for
 from keras.models import load_model
 from keras.preprocessing import image
-import streamlit as st
-from io import StringIO
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from google.cloud import storage
+import tempfile
 
 app = Flask(__name__, template_folder='template')
-
+client = storage.Client.from_service_account_json('keys/serviceaccount.json')
+bucket_name = 'model-loads'
+bucket = client.bucket(bucket_name)
 lists = ["A", "B", "C", "D", "E", "F", "G"]
 
-model = load_model('Model_Saved.h5')
+model = load_model("Model_Saved.h5")
 
 # routes
 @app.route("/")
@@ -21,13 +23,32 @@ def main():
 
 @app.route("/predict", methods = ['GET', 'POST'])
 def predict():
-    # Get uploaded file using local directory
+    if 'file' not in request.files:
+        return 'No file part in the request'
+
     file = request.files['file']
-    image_path = "./images/" + file.filename
-    file.save(image_path)
+
+    if file.filename == '':
+        return 'No selected file'
+
+    # Save temp file to server
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    file.save(temp_file.name)
+    
+	# Save temp file to Google Cloud Storage
+    blob = bucket.blob('images/' + file.filename)
+    blob.upload_from_filename(temp_file.name)
+    # get public url for shown image
+    image_url = blob.public_url
+
+    # Get uploaded file using local directory
+    # file = request.files['file']
+    # image_path = "./images/" + file.filename
+    # file.save(image_path)
+    
     
 	# load image from local
-    image = keras.preprocessing.image.load_img(image_path,target_size=(150,150))
+    image = keras.preprocessing.image.load_img(temp_file.name,target_size=(150,150))
     
 	# image to arrray
     x = keras.preprocessing.image.img_to_array(image)
@@ -43,7 +64,9 @@ def predict():
     
     classify = pred
     # return jsonify({'result': (pred)}, prediction=classify)
-    return render_template('index.html', prediction=classify)
+    temp_file.close()
+    # return to fill in variable website
+    return render_template('index.html', prediction=classify, image_url=image_url)
 if __name__ =='__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
     # app.run(debug = True)
